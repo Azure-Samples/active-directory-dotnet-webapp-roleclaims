88b1c331aa5f171053687babfca209f05a0e1d8e
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-03-20 20:13:28 +0100

    Adding automation to create the AAD application

---
+++ Config.ps1
@@ -0,0 +1,6 @@
+   # Variables for the registration of the AAD application
+    $appName = "TaskTrackerWebApp"
+    $homePage = "https://localhost:44322"
+    $appIdURI = "https://$tenantName/$appName"
+    $logoutURI = "$homePage/Account/EndSession"
+        
